18a045e8b6c56b9cfad4d92a120fef4c2c1331d9
Iwumezie Pamela <chiwumezie@gmail.com>
2024-07-29 18:07:44 +0100

    add coverage

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,15 +1,16 @@
 import { render, screen } from "@testing-library/react"
 import Greet from "./Greet"
 
+describe("Greet", () => {
+    test('expect to render hello world', () => {
+        render(<Greet />)
+        const textElement = screen.getByText(/Hello World/i)
+        expect(textElement).toBeInTheDocument()
+    })
 
-test('expect to render hello world', () => {
-    render(<Greet />)
-    const textElement = screen.getByText(/Hello World/i)
-    expect(textElement).toBeInTheDocument()
-})
-
-test('expect to render hello world with name', () => {
-    render(<Greet name='pamela' />)
-    const textElement = screen.getByText('Hello World pamela')
-    expect(textElement).toBeInTheDocument()
+    test('expect to render hello world with name', () => {
+        render(<Greet name='pamela' />)
+        const textElement = screen.getByText('Hello World pamela')
+        expect(textElement).toBeInTheDocument()
+    })
 })
